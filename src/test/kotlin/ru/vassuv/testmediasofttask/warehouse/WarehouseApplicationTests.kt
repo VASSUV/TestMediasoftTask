16c4fdca5a7818329bf167bbedb904577391923a
vassuv <vassuv@ya.ru>
2025-05-22 20:49:55 +0400

    unzip spring project

---
+++ src/test/kotlin/ru/vassuv/testmediasofttask/warehouse/WarehouseApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.vassuv.testmediasofttask.warehouse
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
